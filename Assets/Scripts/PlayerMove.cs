cabe72b63b939e6e35b1c7ef0626cfab9bffb18b
Tazim Padiyath <tazimpadiyath@gmail.com>
2025-05-11 16:48:14 +0100

    Added a basic grid that spawns when agame starts

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,7 +7,7 @@ public class PlayerMove : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(-5, 0, 0);
     }
 
     // Update is called once per frame
---
+++ Assets/SpawnMaze.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SpawnMaze : MonoBehaviour
+{
+    public GameObject verticalWall;
+    public GameObject horizontalWall;
+
+    private float horizontalWallInitalXPosition = -3.5f;
+    private float verticalWallInitalXPosition = -4.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnGrid()
+    {
+        for (int xValue = 0; xValue <= 7; xValue++)
+        {
+            for (int yValue = -3; yValue <= 3; yValue++)
+            {
+                Instantiate(horizontalWall, new Vector3(horizontalWallInitalXPosition + xValue, yValue, 0), Quaternion.Euler(0, 0, 90));
+            }
+        }
+
+        for (int xValue = 0; xValue <= 8; xValue++)
+        {
+            for (float yValue = -2.5f; yValue <= 2.5; yValue++)
+            {
+                Instantiate(verticalWall, new Vector3(verticalWallInitalXPosition + xValue, yValue, 0), transform.rotation);
+            }
+        }
+    }
+}
+
